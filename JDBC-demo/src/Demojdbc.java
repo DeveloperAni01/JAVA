876ea6c59122075bc1b059631123daafdeff1323
DeveloperAni01 <developerani45@gmail.com>
2026-07-26 01:07:33 +0530

    fetch data

--- JDBC-demo/src/Demojdbc.java
+++ JDBC-demo/src/Demojdbc.java
@@ -17,6 +17,8 @@ public class Demojdbc {
         String url = "jdbc:postgresql://localhost:5433/demo";
         String username = "postgres";
         String password = "postgres123";
+        //query
+        String query = "select * from student";
 
         try {
             //load & register
@@ -25,6 +27,21 @@ public class Demojdbc {
             //create connection
             Connection con = DriverManager.getConnection(url,username,password);
             System.out.println("Connection Establish!");
+
+            //create statement
+             Statement st = con.createStatement();
+
+             //execute statement
+            ResultSet data = st.executeQuery(query);
+           while(data.next()){
+               System.out.print(data.getInt("sid") + "-");
+               System.out.print(data.getString("sname") + "-");
+               System.out.println(data.getInt("marks"));
+           }
+
+            //close the connection
+            con.close();
+            System.out.println("Connection closed!");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
