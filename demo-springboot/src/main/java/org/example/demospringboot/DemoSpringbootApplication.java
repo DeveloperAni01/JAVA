1d9ff6f2712eea260684838ad4abd6d55b249374
DeveloperAni01 <developerani45@gmail.com>
2026-07-27 03:47:54 +0530

    DEmo springboot

---
+++ demo-springboot/src/main/java/org/example/demospringboot/DemoSpringbootApplication.java
@@ -0,0 +1,20 @@
+package org.example.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoSpringbootApplication {
+
+    public static void main(String[] args) {
+       ApplicationContext context =  SpringApplication.run(DemoSpringbootApplication.class, args);
+        System.out.println("Hello I started Spring !!");
+
+       Alien obj = context.getBean(Alien.class);
+        obj.code();
+    }
+
+
+
+}
